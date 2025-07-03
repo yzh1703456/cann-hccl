75a53d9d0a38dd9c287a63df5b66a5b277edbbb1
jh l <lianjh24@mails.thu.edu.cn>
2025-07-03 15:16:13 +0800

    add log in coll_all_gather_ring_executor.cc

--- src/domain/collective_communication/algorithm/impl/coll_executor/coll_all_gather/coll_all_gather_ring_executor.cc
+++ src/domain/collective_communication/algorithm/impl/coll_executor/coll_all_gather/coll_all_gather_ring_executor.cc
@@ -144,6 +144,10 @@ HcclResult CollAllGatherRingExecutor::KernelRun(const OpParam &param, ExecMem &e
     u64 hdCount = hdSize / perDataSize;
 
     HCCL_INFO("hdCount: %d\n", hdCount);
+    HCCL_INFO("hdCount: %d\n", hdCount);
+    HCCL_INFO("hdCount: %d\n", hdCount);
+    HCCL_INFO("hdCount: %d\n", hdCount);
+    HCCL_INFO("hdCount: %d\n", hdCount);
 
     bool isMultiNic = topoType_ == TopoType::TOPO_TYPE_8P_RING && nicList.size() != DEVICE_EIGHT;
     bool innRunRet = isMultiNic && (iterNic == nicList.end());
